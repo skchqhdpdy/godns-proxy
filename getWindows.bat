f463a7bbde5db9c636c3e12c16ac2bec2e8e44d9
skchqhdpdy <aniren@debian.moe>
2026-04-07 10:14:14 +0900

    Update getWindows.bat

--- getWindows.bat
+++ getWindows.bat
@@ -1,5 +1,3 @@
-scp -i "C:\Users\skchqhdpdy\.ssh\oracle.key" "C:\godns\godns.go"  ubuntu@ns3.aodd.xyz:/home/ubuntu/
-scp -i "C:\Users\skchqhdpdy\.ssh\oracle.key" "C:\godns\godns.exe" ubuntu@ns3.aodd.xyz:/home/ubuntu/
-scp -i "C:\Users\skchqhdpdy\.ssh\oracle.key" "C:\godns\godns"     ubuntu@ns3.aodd.xyz:/home/ubuntu/
-ssh -i "C:\Users\skchqhdpdy\.ssh\oracle.key" ubuntu@ns3.aodd.xyz "sudo chmod 777 /home/ubuntu/godns.go /home/ubuntu/godns.exe /home/ubuntu/godns"
+scp -i "C:\Users\skchqhdpdy\.ssh\oracle.key" godns.go godns.exe godns ubuntu@ns3.aodd.xyz:/home/ubuntu/godns/
+ssh -i "C:\Users\skchqhdpdy\.ssh\oracle.key" ubuntu@ns3.aodd.xyz "sudo chmod 777 /home/ubuntu/godns/godns.go /home/ubuntu/godns/godns.exe /home/ubuntu/godns/godns"
 pause
